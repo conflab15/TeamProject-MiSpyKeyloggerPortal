54acafd18cf4d7ef82d47638032feb3c87eee2e6
Connor Clarke <connorclarke1511@hotmail.com>
2021-03-25 21:18:42 +0000

    Added Sorting and Filtering to the Logged Events Report. Users can change the quantity of events per page. Search for key words, filter through times and if the event has been read...

--- MiSpyWebAppMVC/Controllers/MonitorController.cs
+++ MiSpyWebAppMVC/Controllers/MonitorController.cs
@@ -22,13 +22,39 @@ namespace MiSpyWebAppMVC.Controllers
         }
 
         // GET: MonitorManager
-        public async Task<IActionResult> Index(int? pageNumber)    
+        public async Task<IActionResult> Index(int? pageNumber, int pageSize, string searchBy, string sortBy)    
         {
-            var events = from e in _context.LoggedEvent
-                         where e.UserId == User.Identity.Name
-                         select e;
+            var events = from e in _context.LoggedEvent select e; //Getting Events from the Database
 
-            int pageSize = 10;
+            if (!String.IsNullOrEmpty(searchBy))
+            {
+                events = events.Where(x => x.UserId == User.Identity.Name && x.Event.Contains(searchBy)); //Getting all reports which match the search criteria
+            }
+            else
+            {
+                events = events.Where(x => x.UserId == User.Identity.Name).OrderBy(s => s.Id); //Get events in just ID order
+            }
+
+            if (pageSize < 5)
+            {
+                pageSize = 5; //Minimum Page Size is 5 reports per page.
+            }
+
+            switch (sortBy)
+            {
+                case "Time":
+                    events = events.Where(x => x.UserId == User.Identity.Name).OrderBy(s => s.Time); //Ordering by Time of Event
+                    break;
+                case "HasRead":
+                    events = events.Where(x => x.UserId == User.Identity.Name).OrderBy(s => s.HasRead); //Ordering by true and false of events being read in the reporting app.
+                    break;
+                default:
+                    events = events.Where(x => x.UserId == User.Identity.Name); //Default Reporting. 
+                    break;
+            }
+
+            await events.ToListAsync();
+
             return View(await PaginatedList<LoggedEvent>.CreateAsync(events.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
